80b1d72ef5f451fda5481c9552418619c7eb7579
Aggarrote <127203390+Aggarrote@users.noreply.github.com>
2023-09-20 19:12:16 +0200

    torreta
    t

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField, Range(1f, 20f)] float velocity;
+    [SerializeField, Range(1f, 20f)] float timeDestruction;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement(Vector2.up);
+        Destroy();
+    }
+    void Movement(Vector2 bullet)
+    {
+        transform.Translate(bullet * velocity * Time.deltaTime);
+    }
+    void Destroy()
+    {
+        timeDestruction -= Time.deltaTime;
+        if (timeDestruction <= 0)
+            Destroy(gameObject);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
+

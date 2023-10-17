19266a640fd2f64914042d8bc27cdbfa6eeaff5e
informaticaesne <santidiazd@gmail.com>
2023-10-17 12:09:39 +0200

    sadfsagfSF

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -59,7 +59,7 @@ public class Head : MonoBehaviour
         {
             Destroy(collision.gameObject);
             Shrinkage(1);
-            StartCoroutine(ParpadeoTemporal());
+            //StartCoroutine(ParpadeoTemporal());
             if (serpiente != null) //lo activa si esta desactivado
             {
                 serpiente.SetActive(!serpiente.activeSelf);

15b646933f3f8a28241ac2f65f4244ee976c923f
informaticaesne <informatica@esne.es>
2023-11-21 11:24:10 +0100

    ParpadeoTerminado
    .

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,28 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float temporizadorParpadeo = 0.5f;
     private Animator animatorParpadeo;
     void Start()
     {
         animatorParpadeo = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Debug.Log("Funciona");
-            animatorParpadeo.SetBool("Choque", true);
-        }
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Entra");
+        //Debug.Log("Entra");
         if (collision.gameObject.tag == "Enemy")
         {
-            Debug.Log("Funciona");
-            animatorParpadeo.SetBool("Choque", true);
+            //Debug.Log("Funciona");
+            animatorParpadeo.SetBool("Choque", true); // activa la condicion para que la animacion empiece
+            StartCoroutine(DesactivarAnimacion(temporizadorParpadeo)); // corutina para que la booleana cambie a falso
         }
-   }
+    }
+    private IEnumerator DesactivarAnimacion(float temporizador)
+    {
+        yield return new WaitForSeconds(temporizador);
+        animatorParpadeo.SetBool("Choque", false);
+    }
 }

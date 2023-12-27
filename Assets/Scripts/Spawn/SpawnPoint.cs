3f1d46dd5ccd4de764f5fdec5efe0c3957bb8ecf
SDDLOL19 <santidiazd@gmail.com>
2023-12-27 21:25:07 +0100

    LaXMArcaElLugar

--- Assets/Scripts/Spawn/SpawnPoint.cs
+++ Assets/Scripts/Spawn/SpawnPoint.cs
@@ -6,11 +6,6 @@ public class SpawnPoint : MonoBehaviour
 {
     [SerializeField] GameObject prefabEnemy;
 
-    private void Start()
-    {
-        SpawnEnemy();
-    }
-
     public void SpawnEnemy()
     {
         Instantiate(prefabEnemy, transform.position, Quaternion.identity); //spawnea el enemigo

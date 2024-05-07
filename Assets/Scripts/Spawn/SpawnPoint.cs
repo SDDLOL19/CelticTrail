f1f31d147b4bbafbdf185c314d4c19f358913240
informaticaesne <informatica@esne.es>
2024-05-07 16:26:17 +0200

    Cambios

--- Assets/Scripts/Spawn/SpawnPoint.cs
+++ Assets/Scripts/Spawn/SpawnPoint.cs
@@ -11,10 +11,27 @@ public class SpawnPoint : MonoBehaviour
     {
         tiroDeDado = Random.Range(1, 100);
 
-        if (tiroDeDado <= 25)  //Asesino
+        if (tiroDeDado <= 20)  //Asesino
         {
             numeroEnemigo = 1;
         }
+
+        /* //mi intento de poner los enemigos firmado: Darío
+        else if (tiroDeDado > 20 && tiroDeDado < 40) //phooka
+        {
+            numeroEnemigo = 2;
+        }
+
+        else if (tiroDeDado > 40 && tiroDeDado < 60) //Hada
+        {
+            numeroEnemigo = 3;
+        }
+
+        else if (tiroDeDado > 60 && tiroDeDado <= 80) //Fachen
+        {
+            numeroEnemigo = 4;
+        }
+        */
 
         else                   //Arquero
         {

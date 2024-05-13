c58812310f33875b48cea94892e58f7d3c2f606a
SDDLOL19 <santidiazd@gmail.com>
2024-05-13 19:19:02 +0200

    APETECAN

--- Assets/Scripts/Enemigos/Ada.cs
+++ Assets/Scripts/Enemigos/Ada.cs
@@ -6,22 +6,24 @@ public class Ada : Enemy
 {
     protected override void AccionExtraUno()
     {
-        Enemy enemigo = Physics2D.OverlapCircle(transform.position, detectionDistance).GetComponent<Enemy>();
+        //Enemy enemigo = Physics2D.OverlapCircle(transform.position, detectionDistance).GetComponent<Enemy>();
 
-        if (enemigo != null /*&& enemigo.GetComponent<Ada>() == null*/)
-        {
-            Debug.Log("Enemigo encontrao");
+        //if (enemigo != null /*&& enemigo.GetComponent<Ada>() == null*/)
+        //{
+        //    Debug.Log("Enemigo encontrao");
 
-            if (enemigo.GetComponent<Ada>() == null)
-            {
-                CambiarObjetivo(enemigo.transform);
-            }
-        }
+        //    if (enemigo.GetComponent<Ada>() == null)
+        //    {
+        //        CambiarObjetivo(enemigo.transform);
+        //    }
+        //}
 
-        else
-        {
-            CambiarObjetivo(GameManager.player.transform);
-        }
+        //else
+        //{
+        //    CambiarObjetivo(GameManager.player.transform);
+        //}
+
+        CambiarObjetivo(GameManager.player.transform);
     }
 
     protected override void ColisionExtraUno(Collider2D collision)
